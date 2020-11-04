59c701729afd6de5bf0ad6858ef495f0c8e7e5a8
DANIELA PÎNZAR <daniela.pinzar@stud.ubbcluj.ro>
2020-11-04 20:54:33 +0200

    Lab5

---
+++ Pinzar_Daniela_Lab5/Models/ExpenseContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Pinzar_Daniela_Lab5.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options) 
+        : base(options)
+        {
+        }
+        public DbSet<Expense> Expense { get; set; }
+
+        public DbSet<ExpenseDTO> ExpenseDTO { get; set; }
+
+    }
+}
